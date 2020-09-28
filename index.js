c6aa4cb5e62ebc75425a5b35c1b9759801234345
Alexa <alexandraws29@outlook.com>
2020-09-27 18:18:57 -0600

    added questions and installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+const Choices = require('inquirer/lib/objects/choices');
 
-];
+
+// array of questions for user
+function questions() {
+    return inquirer.prompt(
+[
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the Title of your README?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your program.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How would one install your program?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How would you describe the usage of your program?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can one make contributions to your program?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How does one test your program?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please indicate the license you are using for your program.',
+        choices: ['Public Domain', 'Permissive','LGPL', 'Copy Left', 'Proprietary']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.'
+    },
+])};
+
 
 // function to write README file
 function writeToFile(fileName, data) {
